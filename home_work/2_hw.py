52e609fd51aa1cebf5b9d2e97402228cc28c3e13
Maxim_Kozlov <aaxxmm@mail.ru>
2024-07-27 13:00:23 +0300

    First Commit

---
+++ home_work/2_hw.py
@@ -0,0 +1,29 @@
+def task_1():
+    a = 4
+    b = 1.47
+    c = 'Строка'
+    d = [1, 2, 3, 4, 5]
+    e = True
+
+    print(a, type(a))  # int
+    print(b, type(b))  # float
+    print(c, type(c))  # str
+    print(d, type(d))  # list
+    print(e, type(e))  # bool
+
+task_1()
+
+
+def task_2():
+
+    a = [1, 2, 3, 5, 8, 13, 21]
+
+    print('Первые 3 значения списка:', a[:3])
+    print('Список чисел Фибоначчи')
+task_2()# Список чисел Фибоначчи
+
+a = 2
+def task_3(a):
+  return a * 2
+print(task_3(a),"a -", type(a),", task_3 -", type(task_3))
+
---
+++ tests/test_one.py
@@ -0,0 +1,10 @@
+def test_passing():
+    assert (1,2,3,) == (1,2,3)
+
+
+def test_zad1():
+    x = ["a", "b", "c"]
+    y = [1, 2, 3]
+    assert not x == y
+
+
